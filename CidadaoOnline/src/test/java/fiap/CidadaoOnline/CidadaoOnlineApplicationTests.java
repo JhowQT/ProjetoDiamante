a8e106efdd94f7aa9e472b20551d4ede204eea7f
JhowQT <jhon.q.torrez@gmail.com>
2025-09-18 12:11:08 -0300

    Criação do repositorio

---
+++ CidadaoOnline/src/test/java/fiap/CidadaoOnline/CidadaoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.CidadaoOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadaoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
