2747eb98912332dbb7bba303444ba9d7a6c1e9a2
JhowQT <jhon.q.torrez@gmail.com>
2025-09-22 11:05:39 -0300

    teste

---
+++ CidadeOnline/src/main/java/com/br/fiap/CidadeOnline/CidadeOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.CidadeOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeOnlineApplication.class, args);
+	}
+
+}
---
+++ CidadeOnline/src/test/java/com/br/fiap/CidadeOnline/CidadeOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.CidadeOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
